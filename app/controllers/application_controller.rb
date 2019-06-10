508f75419c7939ce735c9e655bf0fdce8c49791c
Carlton Dwayne <carltondgrandison@gmail.com>
2019-06-10 18:59:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/recipies' do
-    @recipes = Recipe.all
-    erb :index
+  get '/recipes' do
+     @recipes = Recipe.all
+     erb :index
+   end
+
+   get '/recipes/new' do
+     @recipe = Recipe.new(params)
+     erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+   get '/recipes/:id' do
+    #  binding.pry
+     @recipe = Recipe.find_by(params[:id])
+
+     erb :show
+   end
+
+  #  post '/recipes' do
+  #    @recipe = Recipe.new
+  #    @recipe.save
+   #
+  #    redirect to "/recipes/#{@recipe.id}"
+  #  end
+
+   get '/recipes/:id/edit' do
+     @recipe = Recipe.find_by(params[:id])
+     erb :edit
+   end
+
+   patch '/recipes/:id' do
+     @recipe = Recipe.find_by_id(params[:id])
+     @recipe.name = params[:name]
+     @recipe.ingredients = params[:ingredients]
+     @recipe.cook_time = pararms[:cook_time]
+     @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+   end
+
+  delete '/recipes/:id' do
+    @recipe= Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
   end
 
 end
